8b1b1b8db0723c77ad79b089e71b61a899c4f594
Thibaut Broggi <broggi_t@epitech.eu>
2016-02-06 01:21:23 +0100

    Add bot connection and very basic rights and plugin
        There is a list of admin that can send commands to the bot
        It's only based on nickName, not on loginName, which is unsecure
        Add a test plugin that display text on the console when it's started
        and stoped
        The admin can ask the bot to quit, to load a plugin, to unload one
        or to list all the loaded plugins

---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+var _ = require('lodash');
+var irc = require("irc");
+var bot = new irc.Client("irc.freenode.net", "embot", {userName: "embot",
+						       realName: "embot",
+						       secure: true,
+						       port: 6697,
+						       autoRejoin: true});
+
+var admins = {emeraude: true}; // TODO: manage login name
+var plugins = {};
+
+function loadPlugin(name) {
+  plugins[name] = require('./plugins/' + name);
+  plugins[name].start();
+}
+
+function unloadPlugin(name) {
+  plugins[name].stop();
+  // TODO: decache it
+  delete plugins[name];
+}
+
+bot.addListener('message', function(from, chan, msg) {
+  if (admins[from.toLowerCase()] === true) {
+    if (msg.match(/^!quit.*$/)) {
+      unloadPlugin('hello');
+      bot.disconnect('bye');
+      process.exit();
+    }
+    else if (msg.match(/^!plugin.*$/)) {
+      var args = msg.split(/[\t ]+/);
+      if (args.length > 1) {
+	if (args[1] == 'stop') {
+	  if (args.length == 2) {
+	    bot.say(chan, 'Usage: !plugin stop <name>'); // TODO: wrap it
+	  }
+	  else {
+	    unloadPlugin(args[2]);
+	  }
+	}
+	else if (args[1] == 'list') {
+	  if (_.size(plugins) == 0)
+	    bot.say(chan, 'No plugin loaded');
+	  else
+	    bot.say(chan, 'Loaded plugins: ' + _.keys(plugins).join(' '));
+	}
+	else if (args[1] == 'start') {
+	  if (args.length == 2) {
+	    bot.say(chan, 'Usage: !plugin start <name>'); // TODO: wrap it
+	  }
+	  else {
+	    loadPlugin(args[2]);
+	  }
+	}
+      }
+      else {
+	bot.say(chan, 'Usage: !plugin stop | start | list');
+      }
+    }
+  }
+});
+
+bot.addListener('error', function(msg) {
+  console.error(msg);
+});
+
+bot.addListener('registered', function() {
+  console.log('connected');
+  loadPlugin('hello');
+  bot.join('#4242', function() {
+    console.log('joined !');
+  });
+});
---
+++ plugins/hello.js
@@ -0,0 +1,10 @@
+exports.version = "1.0.0";
+exports.description = "";
+
+exports.start = function() {
+  console.log('started');
+};
+
+exports.stop = function() {
+  console.log('stop it !');
+};
